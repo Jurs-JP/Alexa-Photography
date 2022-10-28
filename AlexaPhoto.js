d3e1137dd42daae2f3abea8cc82854ce39a71933
Jurist John Plaza <40057871+Jurs-JP@users.noreply.github.com>
2022-10-28 20:10:17 +0800

    Add files via upload

--- AlexaPhoto.js
+++ AlexaPhoto.js
@@ -1,1 +1,48 @@
+// const spread = document.getElementById('spreadCards');
+// const cd1 = document.getElementById("card1");
+// const cd2 = document.getElementById("card2");
+// const cd3 = document.getElementById("card3");
+// const cd4 = document.getElementById("card4");
 
+
+// window.addEventListener("scroll", (key)=> {
+	
+// 	if(scroll == 100){
+// cd1.style.transform = "translate(320px) rotate(4deg)";
+// cd2.style.transform = "translate(-300px) rotate(-1deg)";
+// cd3.style.transform = "translate(150px) rotate(-2deg)";
+// cd4.style.transform = "translate(-105px) rotate(6deg)";
+	
+// 	}
+
+// })
+
+
+// var zIn = 12;
+// 	var allCards = document.querySelectorAll("div.card");
+	
+// 	for(let card of allCards){
+		
+// 		card.addEventListener("click", ()=> {
+
+		
+// 	cd1.style.transform = "translate(320px) rotate(4deg)";
+// 	cd2.style.transform = "translate(-300px) rotate(-1deg)";
+
+// 	cd3.style.transform = "translate(150px) rotate(-2deg)";
+// 	cd4.style.transform = "translate(-105px) rotate(6deg)";	
+	
+// 		zIn++
+// 	card.style.transform = "translate(1px) scale(1.2)" ; 
+// 	card.style.zIndex = zIn;
+		
+// 		})
+// 		card.addEventListener("dblclick", ()=>{
+// 			cd1.style.transform = "translate(320px) rotate(4deg)";
+// 			cd2.style.transform = "translate(-300px) rotate(-1deg)";
+			
+// 			cd3.style.transform = "translate(150px) rotate(-2deg)";
+// 			cd4.style.transform = "translate(-105px) rotate(6deg)";
+// 				})
+// 	}
+	
