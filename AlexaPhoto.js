112ed47bc352007dfc6495315ff69e3f148af70a
Jurist John Plaza <40057871+Jurs-JP@users.noreply.github.com>
2022-10-29 18:54:40 +0800

    Add files via upload

--- AlexaPhoto.js
+++ AlexaPhoto.js
@@ -1,48 +1,42 @@
-// const spread = document.getElementById('spreadCards');
-// const cd1 = document.getElementById("card1");
-// const cd2 = document.getElementById("card2");
-// const cd3 = document.getElementById("card3");
-// const cd4 = document.getElementById("card4");
+const spread = document.getElementById('spreadCards');
+const cd1 = document.getElementById("card1");
+const cd2 = document.getElementById("card2");
+const cd3 = document.getElementById("card3");
+const cd4 = document.getElementById("card4");
 
 
-// window.addEventListener("scroll", (key)=> {
+var zIn = 12;
+	var allCards = document.querySelectorAll("div.card");
 	
-// 	if(scroll == 100){
-// cd1.style.transform = "translate(320px) rotate(4deg)";
-// cd2.style.transform = "translate(-300px) rotate(-1deg)";
-// cd3.style.transform = "translate(150px) rotate(-2deg)";
-// cd4.style.transform = "translate(-105px) rotate(6deg)";
-	
-// 	}
-
-// })
-
-
-// var zIn = 12;
-// 	var allCards = document.querySelectorAll("div.card");
-	
-// 	for(let card of allCards){
+	for(let card of allCards){
 		
-// 		card.addEventListener("click", ()=> {
+		card.addEventListener("click", ()=> {
 
 		
-// 	cd1.style.transform = "translate(320px) rotate(4deg)";
-// 	cd2.style.transform = "translate(-300px) rotate(-1deg)";
+	cd1.style.transform = "translate(25vw) rotate(4deg)";
+	cd2.style.transform = "translate(-25vw) rotate(-1deg)";
 
-// 	cd3.style.transform = "translate(150px) rotate(-2deg)";
-// 	cd4.style.transform = "translate(-105px) rotate(6deg)";	
+	cd3.style.transform = "translate(9vw) rotate(-2deg)";
+	cd4.style.transform = "translate(-9vw) rotate(6deg)";	
 	
-// 		zIn++
-// 	card.style.transform = "translate(1px) scale(1.2)" ; 
-// 	card.style.zIndex = zIn;
+		zIn++
+	card.style.transform = "translate(1px) scale(1.4)" ; 
+
+	card.style.zIndex = zIn;
 		
-// 		})
-// 		card.addEventListener("dblclick", ()=>{
-// 			cd1.style.transform = "translate(320px) rotate(4deg)";
-// 			cd2.style.transform = "translate(-300px) rotate(-1deg)";
-			
-// 			cd3.style.transform = "translate(150px) rotate(-2deg)";
-// 			cd4.style.transform = "translate(-105px) rotate(6deg)";
-// 				})
-// 	}
+		})
+		card.addEventListener("dblclick", ()=>{
+			cd1.style.transform = "translate(25vw) rotate(4deg)";
+			cd2.style.transform = "translate(-25vw) rotate(-1deg)";
+
+			cd3.style.transform = "translate(9vw) rotate(-2deg)";
+			cd4.style.transform = "translate(-9vw) rotate(6deg)";		
+				})
+	}
 	
+
+
+var card= {
+	card1: "photo1",
+	card2: ""
+}
